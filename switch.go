1a68476137e4378102d6a99155909789fa0f24b4
Zhu <dayi.zhu@intel.com>
2020-07-18 15:09:54 +0800

    +ifElse.go, switch.go

---
+++ switch.go
@@ -0,0 +1,41 @@
+package main
+import (
+	"fmt"
+	"time"
+)
+
+func main(){
+
+	i := 2
+	fmt.Println("Write ", i, " as ")
+	switch i {
+	case 1:
+		fmt.Println("one")
+	case 2:
+		fmt.Println("two")
+	case 3:
+		fmt.Println("three")
+	}
+
+	switch time.Now().Weekday() {
+	case time.Saturday, time.Sunday:
+		fmt.Println("It's the weekend")
+	default:
+		fmt.Println("It's a weekday")
+	}
+
+	whatAmI := func(i interface{}){
+		switch t := i.(type) {
+		case bool:
+			fmt.Println("I am a bool")
+		case int:
+			fmt.Println("I am an int")
+		default:
+			fmt.Printf("Don't know type %T\n", t)
+		}
+	}
+
+	whatAmI(true)
+	whatAmI(1)
+	whatAmI("hey")
+}
