634fc881953d54d397e9b6b8dabbea3367f6d5f3
Zhu <dayi.zhu@intel.com>
2020-07-22 05:53:52 +0800

    url-parsing.go sha1-hashes.go base64-encoding.go

---
+++ base64-encoding.go
@@ -0,0 +1,18 @@
+package main
+import(
+	"fmt"
+	"encoding/base64"
+)
+func main() {
+	data := "abc123!?$*&()'-=@~"
+
+	sEnc := base64.StdEncoding.EncodeToString([]byte(data))
+	fmt.Println(sEnc)
+	sDec, _ := base64.StdEncoding.DecodeString(sEnc)
+	fmt.Println(string(sDec), "\n")
+
+	uEnc := base64.URLEncoding.EncodeToString([]byte(data))
+	fmt.Println(uEnc)
+	uDec, _ := base64.URLEncoding.DecodeString(uEnc)
+	fmt.Println(string(uDec), "\n")
+}
---
+++ url-parsing.go
@@ -0,0 +1,28 @@
+package main
+import(
+	"fmt"
+	"net"
+	"net/url"
+)
+func main() {
+	s := "postgres://user:pass@host.com:5432/path?k=v#f"
+
+	u, err := url.Parse(s)
+	if err != nil{
+		panic(err)
+	}
+	fmt.Println(u.Scheme, u.User, u.User.Username())
+	p, _ := u.User.Password()
+	fmt.Println(p)
+
+	fmt.Println(u.Host)
+	host, port, _ := net.SplitHostPort(u.Host)
+	fmt.Println(host, port)
+
+	fmt.Println(u.Path)
+	fmt.Println(u.Fragment)
+	fmt.Println(u.RawQuery)
+	m, _ := url.ParseQuery(u.RawQuery)
+	fmt.Println(m)
+	fmt.Println(m["k"][0])
+}
