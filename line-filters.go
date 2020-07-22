fd86831cfb53882aff758210bf5da5f6a1ce5400
Zhu <dayi.zhu@intel.com>
2020-07-23 04:49:40 +0800

    line-filters.go

---
+++ line-filters.go
@@ -0,0 +1,18 @@
+package main
+import(
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan(){
+		uc1 := strings.ToUpper(scanner.Text())
+		fmt.Println(uc1)
+	}
+	if err := scanner.Err(); err != nil{
+		fmt.Println(os.Stdout, "error:", err)
+		os.Exit(1)
+	}
+}
