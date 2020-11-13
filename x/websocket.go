45c994bb1d5f7d294f0eba3614dad0866b700642
u2004 <zhudy@yahoo.com>
2020-11-13 08:06:29 +0800

    example for golang.org/x/net/websocket

---
+++ x/websocket.go
@@ -0,0 +1,49 @@
+//https://blog.csdn.net/yjp19871013/article/details/83444148
+package main
+ 
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"strings"
+ 
+	"golang.org/x/net/websocket"
+)
+ 
+func upper(ws *websocket.Conn) {
+	var err error
+	for {
+		var reply string
+ 
+		if err = websocket.Message.Receive(ws, &reply); err != nil {
+			fmt.Println(err)
+			continue
+		}
+ 
+		if err = websocket.Message.Send(ws, strings.ToUpper(reply)); err != nil {
+			fmt.Println(err)
+			continue
+		}
+	}
+}
+ 
+func index(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		return
+	}
+ 
+	t, _ := template.ParseFiles("index.html")
+	t.Execute(w, nil)
+}
+ 
+func main() {
+	http.Handle("/upper", websocket.Handler(upper))
+	http.HandleFunc("/", index)
+ 
+	if err := http.ListenAndServe(":9999", nil); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+}
+
