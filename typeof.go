6893acf9a39d93e92575706fbc3b17444b7ffe6f
u2004 <zhudy@yahoo.com>
2020-11-26 19:24:56 +0800

    golang获取变量数据类型

---
+++ typeof.go
@@ -0,0 +1,13 @@
+//https://studygolang.com/articles/14269
+package main
+import (
+    "reflect"
+    "fmt"
+)
+func main() {
+    v := "hello world"
+    fmt.Println(typeof(v))
+}
+func typeof(v interface{}) string {
+    return reflect.TypeOf(v).String()
+}
